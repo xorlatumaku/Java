8fbefb1fe433819b376963fa9cddb860d31781c7
xorla <xorlatumaku@gmail.com>
2026-09-04 22:54:55 -0700

    Compute area

---
+++ Chapter-02/ComputeArea.java
@@ -0,0 +1,12 @@
+public class ComputeArea {
+  public static void main(String[] args) {
+    double radius;
+    double area;
+
+    radius = 20;
+    area = radius * radius * 3.14159;
+
+    System.out.println("The area of the cirlce with radius " + 
+        radius + " is " + area);
+  }
+}
