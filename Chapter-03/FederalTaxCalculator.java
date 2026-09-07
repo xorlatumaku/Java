37af97af9bdf2d05535e0fe0c2d52f0fc35668a2
xorla <xorlatumaku@gmail.com>
2026-09-07 07:05:55 -0700

    2025 U.S. Federal Personal Income Tax Calculator

---
+++ Chapter-03/FederalTaxCalculator.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/** 
+ * 2025 U.S. Federal Personal Income Tax Calculator
+ * Computes tax owed using marginal tax brackets for four filing statuses:
+ *   1. Single
+ *   2. Married Filing Jointly (or Qualifying Widow(er))
+ *   3. Married Filing Separately
+ *   4. Head of Household
+ *
+ * Brackets reflect the 2025 IRS tax tables (tax year 2025, filed in 2026).
+ */ 
+
+public class FederalTaxCalculator {
+  
+}
