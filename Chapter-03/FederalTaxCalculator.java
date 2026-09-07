2e0482518562b6203b5047edd5c310a597d0f5ef
xorla <xorlatumaku@gmail.com>
2026-09-07 07:10:55 -0700

    Add lowerbound and rate for SINGLE bracket

--- Chapter-03/FederalTaxCalculator.java
+++ Chapter-03/FederalTaxCalculator.java
@@ -12,5 +12,9 @@ import java.util.Scanner;
  */ 
 
 public class FederalTaxCalculator {
-  
+  // Each bracket: { lowerBound, rate } -- rate as a decimal (0.10 = 10%)
+  private static final double[][] SINGLE = {
+    {0, 0.10}, {11925, 0.12}, {48475, 0.22}, {103350, 0.24},
+    {197300, 0.32}, {250525, 0.35}, {626350, 0.37}
+  };
 }
