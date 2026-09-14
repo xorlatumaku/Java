37bb7fd8ebf0bdc41026927b5f70162ce92f33f5
xorla <xorlatumaku@gmail.com>
2026-09-14 03:36:37 -0700

    Add nested for loops to display a multiplication table

---
+++ Chapter-05/MultiplicationTable.java
@@ -0,0 +1,24 @@
+public class MultiplicationTable {
+  public static void main(String[] args) {
+    System.out.println("        MultiplicationTable");
+
+    // Display the number title
+    System.out.print("    ");
+    for (int j = 1; j <= 9; j++) {
+      System.out.print("    " + j);
+
+    }
+      System.out.println("\n-----------------------------------------------------");
+
+      // Display table body
+      for (int i = 1; i <= 9; i++) {
+        System.out.print(i + "  | ");
+        for (int j = 1; j <= 9; j++) {
+          // Display the product and align properly
+          System.out.printf("%5d", i * j);
+        }
+        System.out.println();
+      }
+    }
+  }
+
