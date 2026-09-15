75b86a3b75b3266428c987370b77badffa42feec
xorla <xorlatumaku@gmail.com>
2026-09-14 23:49:07 -0700

    Repeatedly find prime numbers

--- Chapter-05/PrimeNumber.java
+++ Chapter-05/PrimeNumber.java
@@ -1,5 +1,24 @@
 public class PrimeNumber {
   public static void main(String[] args) {
     final int NUMBER_OF_PRIMES = 50;
+    final int NUMBER_OF_PRIMES_PER_LINE = 10;
+    int count = 0;
+    int number = 2;
+
+    System.out.println("The first 50 prime numbers are \n");
+
+    // Repeatedly find prime numbers
+    while (count < NUMBER_OF_PRIMES) {
+      // Assume the number is prime
+      boolean isPrime = true; // Is the current number prime?
+
+      // Test whether number is prime
+      for (int divisor = 2; divisor <= number / 2; divisor++) {
+        if (number % divisor == 0) {    // If true, number is not prime
+          isPrime = false;  // Set isPrime to false
+          break;
+        }
+      }
+    }
   }
 }
