bfd05b6a73a09bc1d7a7d532d664c1077e1ee44d
xorla <xorlatumaku@gmail.com>
2026-09-16 01:16:32 -0700

    Prompt the user to enter year

---
+++ Chapter-06/PrintCalendar.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PrintCalendar {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    // Prompt the user to enter year
+    System.out.print("Enter full year (e.g., 2012): ");
+    int year = input.nextInt();
+  }
+}
