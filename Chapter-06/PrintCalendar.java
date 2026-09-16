1a2ea5819a52b207a581e38fa60ea6c40c8ac6d0
xorla <xorlatumaku@gmail.com>
2026-09-16 01:19:10 -0700

    Prompt the user to enter month

--- Chapter-06/PrintCalendar.java
+++ Chapter-06/PrintCalendar.java
@@ -7,5 +7,9 @@ public class PrintCalendar {
     // Prompt the user to enter year
     System.out.print("Enter full year (e.g., 2012): ");
     int year = input.nextInt();
+
+    // Prompt the user to enter month
+    System.out.print("Enter month as a number between 1 and 12: ");
+    int month = input.nextInt();
   }
 }
