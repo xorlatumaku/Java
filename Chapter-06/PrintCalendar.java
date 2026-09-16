a06acfcc5eaca7efcd8b8a0aff4f4a0273bfccb2
xorla <xorlatumaku@gmail.com>
2026-09-16 01:41:21 -0700

    Get the English name for the month

--- Chapter-06/PrintCalendar.java
+++ Chapter-06/PrintCalendar.java
@@ -31,4 +31,25 @@ public class PrintCalendar {
     System.out.println("--------------------------------------------");
     System.out.println(" Sun Mon Tue Wed Thu Fri Sat");
   }
+
+  /* Get the English name for the month */
+  public static String getMonthName(int month) {
+    String monthName = "";
+    switch (month) {
+      case 1: monthName= "January"; break;
+      case 2: monthName= "February"; break;
+      case 3: monthName= "March"; break;
+      case 4: monthName= "April"; break;
+      case 5: monthName= "May"; break;
+      case 6: monthName= "June"; break;
+      case 7: monthName= "July"; break;
+      case 8: monthName= "August"; break;
+      case 9: monthName= "September"; break;
+      case 10: monthName= "October"; break;
+      case 11: monthName= "November"; break;
+      case 12: monthName= "December";
+    }
+
+    return monthName;
+  }
 }
