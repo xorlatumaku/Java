853075f194af1fbb3181ed56934973d25e8f6339
xorla <xorlatumaku@gmail.com>
2026-09-17 03:45:22 -0700

    Add linear search

---
+++ Chapter-07/LinearSearch.java
@@ -0,0 +1,9 @@
+public class LinearSearch {
+  public static int LinearSearch(int[] list, int key) {
+    for (int i = 0; i < list.length; i++) {
+      if (key == list[i])
+        return i;
+    }
+    return -1;
+  }
+}
