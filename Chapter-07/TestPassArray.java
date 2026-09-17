8ae734d673c43d8773cbc2f4b4a3caf60862c50d
xorla <xorlatumaku@gmail.com>
2026-09-17 01:17:18 -0700

    Swap elements using swap method

---
+++ Chapter-07/TestPassArray.java
@@ -0,0 +1,12 @@
+public class TestPassArray {
+  public static void main(String[] args) {
+    int[] a = {1, 2};
+
+    // Swap elements using the swap method
+    System.out.println("Before invoking swap");
+    System.out.println("array is {" + a[0] + ", " + a[1] + "}");
+    swap(a[0], a[1]);
+    System.out.println("After invoking swap");
+    System.out.println("array is {" + a[0] + ", " + a[1] + "}");
+  }
+}
