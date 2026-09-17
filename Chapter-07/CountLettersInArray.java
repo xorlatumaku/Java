3edfa71faf1b9a568401a15da8b41cf7498b3705
xorla <xorlatumaku@gmail.com>
2026-09-17 02:21:43 -0700

    Create lowercase letters randomly and assign them to the array

--- Chapter-07/CountLettersInArray.java
+++ Chapter-07/CountLettersInArray.java
@@ -19,6 +19,10 @@ public class CountLettersInArray {
   // Create an array of characters
   public static char[] createArray() {
     // Declare an array of characters and create it
-    char[] chars = new char[100]; 
+    char[] chars = new char[100];
+
+    // Create lowercase letters randomly and assign them to the array
+    for (int i = 0; i < chars.length; i++)
+      chars[i] = RandomCharacter.getRandomLowerCaseLetter();
   }
 }
