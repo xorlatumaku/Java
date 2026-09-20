d2843c85e1206bffd104312b9a2bdf6622e4208e
xorla <xorlatumaku@gmail.com>
2026-09-20 01:37:08 -0700

    Create a circle with radius 1

---
+++ Chapter-09/TestCircle.java
@@ -0,0 +1,9 @@
+public class TestCircle {
+  public static void main(String[] args) {
+    // Create a circle with radius 1
+    Circle circle1 = new Circle();
+    System.out.println("The area of the circle of radius "
+        + circle1.radius + " is " + circle1.getArea());
+
+  }
+}
