c1258df09c2ce46998721c69397bd549faa1ff75
xorla <xorlatumaku@gmail.com>
2026-09-20 01:41:34 -0700

    Create a circle with radius 125

--- Chapter-09/TestCircle.java
+++ Chapter-09/TestCircle.java
@@ -9,5 +9,10 @@ public class TestCircle {
     Circle circle2 = new Circle(25);
     System.out.println("The area of the circle of radius "
         + circle2.radius + "is" + circle2.getArea());
+
+    // Create a circle with radius 125
+    Circle circle3 = new Circle(125);
+    System.out.println("The area of the circle of radius "
+        + circle3.radius + " is " + circle3.getArea());
   }
 }
