80f182000dc640d4bd3d60dd8789a620a38918ca
xorla <xorlatumaku@gmail.com>
2026-09-21 03:04:44 -0700

    Display c1 and c2 AFTER c2 was created

--- Chapter-09/TestStaticCircle.java
+++ Chapter-09/TestStaticCircle.java
@@ -16,5 +16,12 @@ public class TestStaticCircle {
     
     // Modify c1
     c1.radius = 9;
+
+    // Display c1 and c2 AFTER c2 was created
+    System.out.println("\nAfter creating c2 and modifying c1");
+    System.out.println("c1: radius (" + c1.radius +
+        ") and number of Circle objects (" + c1.numberOfObjects + ")");
+    System.out.println("c2: radius (" + c2.radius +
+        ") and number of Circle objects (" + c2.numberOfObjects + ")");
   }
 }
