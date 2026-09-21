3212ca6e45bb626b908fc8943fb87a86f4a76529
xorla <xorlatumaku@gmail.com>
2026-09-21 02:30:30 -0700

    Construct a circle with radius 1

--- Chapter-09/StaticCircle.java
+++ Chapter-09/StaticCircle.java
@@ -3,4 +3,10 @@ public class Cirlce {
 
   // The number of objects created
   static int numberOfObjects = 0;
+
+  // Construct a circle with radius 1
+  Cirlce() {
+    radius = 1;
+    numberOfObjects++;
+  }
 }
