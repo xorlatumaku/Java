491e2ad9146eb16b2cfbed940d145ba7b4e5342d
xorla <xorlatumaku@gmail.com>
2026-09-22 03:41:52 -0700

    Enter annual interest rate

--- Chapter-10/TestLoanClass.java
+++ Chapter-10/TestLoanClass.java
@@ -5,4 +5,10 @@ public class TestLoanClass {
     // Create a Scanner
     Scanner input = new Scanner(System.in);
   }
+
+  // Enter annual interest rate
+  System.out.print("Enter annual interest rate: ");
+  double annualInterestRate = input.nextDouble();
+
+ 
 }
