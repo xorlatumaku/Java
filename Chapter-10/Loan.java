e8b319a39f4e0cbe39045e4caa13400c947138b7
xorla <xorlatumaku@gmail.com>
2026-09-22 03:23:13 -0700

    Add monthly payment

--- Chapter-10/Loan.java
+++ Chapter-10/Loan.java
@@ -46,4 +46,12 @@ public class Loan {
   public void setLoanAmount(double loanAmount) {
     this.loanAmount = loanAmount;
   }
+
+  // Find monthly payment
+  public double getMonthlyPayment() {
+    double monthlyInterestRate = annualInterestRate / 1200;
+    double monthlyPayment = loanAmount * monthlyInterestRate / 
+      (1 - (1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12)));
+    return monthlyPayment;
+  }
 }
