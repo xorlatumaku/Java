6f9db09467b3367639beef8fd8090e17b9e3b7ae
xorla <xorlatumaku@gmail.com>
2026-09-22 03:24:42 -0700

    Add total payment

--- Chapter-10/Loan.java
+++ Chapter-10/Loan.java
@@ -54,4 +54,10 @@ public class Loan {
       (1 - (1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12)));
     return monthlyPayment;
   }
+
+  // Find total payment
+  public double getTotalPayment() {
+    double totalPayment = getMonthlyPayment() * numberOfYears * 12;
+    return totalPayment;
+  }
 }
