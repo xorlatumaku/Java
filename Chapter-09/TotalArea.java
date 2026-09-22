b2e6e2f5d26aef022e5fbbbc237350b565ed5b22
xorla <xorlatumaku@gmail.com>
2026-09-21 23:23:35 -0700

    Create an array of Circle objects

--- Chapter-09/TotalArea.java
+++ Chapter-09/TotalArea.java
@@ -6,4 +6,16 @@ public class TotalArea {
     // Print circleArray and total areas of the circles
     printCircleArray(circleArray);
   }
+
+  // Create an array of Circle objects
+  public static Circle[] createCircleArray() {
+    Circle[] circleArray = new Circle[5];
+
+    for (int i = 0; i < circleArray.length; i++) {
+      circleArray[i] = new Circle(Math.random() * 100);
+    }
+
+    // Return Circle array
+    return circleArray;
+  }
 }
