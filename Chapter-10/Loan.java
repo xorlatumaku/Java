20ef97faf12495910e48c9c87910f020497c2f79
xorla <xorlatumaku@gmail.com>
2026-09-22 02:52:32 -0700

    Add default constructor

---
+++ Chapter-10/Loan.java
@@ -0,0 +1,11 @@
+public class Loan {
+  private double name;
+  private int  numberOfYears;
+  private double loanAmount;
+  private java.util.Date loanDate;
+
+  // Default constructor
+  public Loan() {
+    this(2.5, 1, 1000);
+  }
+}
