19f8fa4696a3c9c4972ced1885900b41f501e94c
xorla <xorlatumaku@gmail.com>
2026-09-21 22:52:20 -0700

    Increase myCircle's radius by 10%

--- Chapter-09/TestPrivateCircle.java
+++ Chapter-09/TestPrivateCircle.java
@@ -4,5 +4,13 @@ public class TestPrivateCircle {
     PrivateCircle myCircle = new PrivateCircle(5.0);
     System.out.println("The area of the circle of radius "
         + myCircle.getRadius() + " is " + myCircle.getArea());
-  }
+
+    // Increase myCircle's radius by 10%
+    myCircle.setRadius(myCircle.getRadius() * 1.1);
+    System.out.println("The area of the circle of radius "
+        + myCircle.getRadius() + " is " + myCircle.getArea());
+
+    System.out.println("The number of objects created is "
+        + PrivateCircle.getNumberOfObjects());
+  } 
 }
