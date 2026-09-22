b6807c5219ee0b1ca30ec146917071184e68cdc7
xorla <xorlatumaku@gmail.com>
2026-09-22 06:25:08 -0700

    Create an object

--- Chapter-10/UseBMIClass.java
+++ Chapter-10/UseBMIClass.java
@@ -3,5 +3,7 @@ public class UseBMIClass {
     BMI bmi1 = new BMI("Kim Yang", 18, 145, 70);
     System.out.println("The BMI for " + bmi1.getName() + " is "
         + bmi1.getBMI() + " " + bmi1.getStatus());
+
+    BMI bmi2 = new BMI("Susan King", 215, 70);
   }
 }
