ca74a0bf658a297beb2cdfb66d3ed6ce5db8853f
xorla <xorlatumaku@gmail.com>
2026-09-22 06:26:18 -0700

    Invoke instance method

--- Chapter-10/UseBMIClass.java
+++ Chapter-10/UseBMIClass.java
@@ -5,5 +5,7 @@ public class UseBMIClass {
         + bmi1.getBMI() + " " + bmi1.getStatus());
 
     BMI bmi2 = new BMI("Susan King", 215, 70);
+    System.out.println("The BMI for " + bmi2.getName() + " is "
+        + bmi2.getBMI() + " " + bmi2.getStatus());
   }
 }
