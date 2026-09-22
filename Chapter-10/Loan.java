31527dc8ffffacad6a91b185c18f7f8dce745e83
xorla <xorlatumaku@gmail.com>
2026-09-22 02:56:48 -0700

    Construct a loan with specified annual interest rate, number of years, and loan amount

--- Chapter-10/Loan.java
+++ Chapter-10/Loan.java
@@ -8,4 +8,12 @@ public class Loan {
   public Loan() {
     this(2.5, 1, 1000);
   }
+
+  // Construct a loan with specified annual interest rate, number of years, and loan amount
+  public Loan(double annualInterestRate, int numberOfYears, double loanAmount) {
+    this.annualInterestRate = annualInterestRate;
+    this.numberOfYears = numberOfYears;
+    this.loanAmount = loanAmount;
+    loanDate = new java.util.Date();
+  }
 }
