531df929653d9330418223332d1af3e5b7e5bd59
xorla <xorlatumaku@gmail.com>
2026-09-22 23:22:07 -0700

    Add a course

--- Chapter-10/Course.java
+++ Chapter-10/Course.java
@@ -2,4 +2,8 @@ public class Course {
   private String courseName;
   private String[] students = new String[100];
   private int numberOfStudents;
+
+  public Course (String courseName) {
+    this.courseName = courseName;
+  }
 }
