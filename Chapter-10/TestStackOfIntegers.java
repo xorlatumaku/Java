7a38a0d330b2c5ce241b3bb6951ccf16a5881a2c
xorla <xorlatumaku@gmail.com>
2026-09-23 02:11:34 -0700

    Push to stack

--- Chapter-10/TestStackOfIntegers.java
+++ Chapter-10/TestStackOfIntegers.java
@@ -1,5 +1,8 @@
 public class TestStackOfIntegers {
   public static void main(String[] args) {
     StackOfIntegers stack = new StackOfIntegers();
+
+    for (int i = 0; i < 10; i++)
+      stack.push(i);
   }
 }
