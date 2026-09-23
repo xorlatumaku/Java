a199f80291875af1054e3b16510f749fd44363d5
xorla <xorlatumaku@gmail.com>
2026-09-22 23:36:45 -0700

    Create a Course

---
+++ Chapter-10/TestCourse.java
@@ -0,0 +1,7 @@
+public class TestCourse {
+  public static void main(String[] args) {
+    Course course1 = new Course("Data Structures");
+    Course course2 = new Course("Database Systems");
+
+  }
+}
