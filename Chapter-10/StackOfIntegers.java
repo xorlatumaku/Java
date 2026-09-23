0d7a01f01cde43e9776b2d94700270fe6d2458f3
xorla <xorlatumaku@gmail.com>
2026-09-23 02:06:00 -0700

    Return the top element from the stack

--- Chapter-10/StackOfIntegers.java
+++ Chapter-10/StackOfIntegers.java
@@ -28,4 +28,9 @@ public class StackOfIntegers {
   public int pop() {
     return elements[--size];
   }
+
+  // Return the top element from the stack
+  public int peek() {
+    return elements[size - 1];
+  }
 }
