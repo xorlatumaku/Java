e12a89a47fbd21ee48f36a2b823adf14d002dc62
xorla <xorlatumaku@gmail.com>
2026-09-22 23:38:31 -0700

    Add a Student

--- Chapter-10/TestCourse.java
+++ Chapter-10/TestCourse.java
@@ -2,6 +2,12 @@ public class TestCourse {
   public static void main(String[] args) {
     Course course1 = new Course("Data Structures");
     Course course2 = new Course("Database Systems");
+    
+    course1.addStudent("Peter Jones");
+    course1.addStudent("Kim Smith");
+    course1.addStudent("Anne Kennedy");
 
+    course2.addStudent("Peter Jones");
+    course2.addStudent("Steve Smith");
   }
 }
