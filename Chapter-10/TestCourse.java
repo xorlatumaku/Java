8b7f427c4d9d1d6806c03f5f28af6fcd16bb7816
xorla <xorlatumaku@gmail.com>
2026-09-22 23:52:35 -0700

    Return the number of students and their names in both courses

--- Chapter-10/TestCourse.java
+++ Chapter-10/TestCourse.java
@@ -9,5 +9,20 @@ public class TestCourse {
 
     course2.addStudent("Peter Jones");
     course2.addStudent("Steve Smith");
+
+    System.out.println("Number of students in Data Structures: " + course1.getNumberOfStudents());
+    String[] students = course1.getStudents();
+
+    for (int i = 0; i < course1.getNumberOfStudents(); i++) 
+      System.out.print(students[i] + ", ");
+
+    System.out.println();
+    System.out.println("Number of students in Database Systems: " + course2.getNumberOfStudents());
+    
+    String[] dbStudents = course2.getStudents();
+    for (int i = 0; i < course2.getNumberOfStudents(); i++)
+      System.out.print(dbStudents[i] + ", ");
+
+    System.out.println();
   }
 }
