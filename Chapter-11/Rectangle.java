738fa12baf59e92927297ceac779448d611a3592
xorla <xorlatumaku@gmail.com>
2026-09-24 02:12:31 -0700

    Add rectangle constructor

--- Chapter-11/Rectangle.java
+++ Chapter-11/Rectangle.java
@@ -1,5 +1,19 @@
 public class Rectangle extends GeometricObject {
   private double width;
   private double height;
-
+  
+  public Rectangle() {
   }
+
+  public Rectangle(double width, double height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  public Rectangle(double width, double height, String color, boolean filled) {
+    this.width = width;
+    this.height = height;
+    setColor(color);
+    setFilled(filled);
+  }
+}
