27e1e6f9b90a8abf2b895a1db12452afdd776cb5
xorla <xorlatumaku@gmail.com>
2026-09-23 22:38:29 -0700

    Prompt the user to enter a string

--- Chapter-10/PalindromeIgnoreNonAlphanumeric.java
+++ Chapter-10/PalindromeIgnoreNonAlphanumeric.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class PalindromeIgnoreNonAlphanumeric {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
+
+    // Prompt the user to enter a string
+    System.out.print("Enter a string: ");
+    String s = input.nextLine();
   }
 }
