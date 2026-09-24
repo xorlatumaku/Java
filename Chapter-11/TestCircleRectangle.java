296b6d0e8a146bed5ca5a4a657f5b253634c2943
xorla <xorlatumaku@gmail.com>
2026-09-24 02:45:13 -0700

    Create Rectangle object

--- Chapter-11/TestCircleRectangle.java
+++ Chapter-11/TestCircleRectangle.java
@@ -6,5 +6,7 @@ public class TestCircleRectangle {
     System.out.println("The radius is " + circle.getRadius());
     System.out.println("The area is " + circle.getArea());
     System.out.println("The diameter is " + circle.getDiameter());
+
+    Rectangle rectangle = new Rectangle(2, 4);
   }
 }
