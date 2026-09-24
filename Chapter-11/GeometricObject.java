f61a358b36067bf61aef48a9ac7326b82d8c2a06
xorla <xorlatumaku@gmail.com>
2026-09-24 01:20:29 -0700

    Construct a default geometric object

--- Chapter-11/GeometricObject.java
+++ Chapter-11/GeometricObject.java
@@ -2,4 +2,9 @@ public class GeometricObject {
   private String color = "white";
   private boolean filled;
   private java.util.Date dateCreated;
+
+  // Construct a default geometric object
+  public GeometricObject() {
+    dateCreated = new java.util.Date();
+  }
 }
