f96f68a9ac6f5b2b1fb60bb5ec3638ac8c891b1c
xorla <xorlatumaku@gmail.com>
2026-09-25 02:05:38 -0700

    Fix typo

--- Chapter-11/PolymorphismDemo.java
+++ Chapter-11/PolymorphismDemo.java
@@ -4,10 +4,10 @@ public class PolymorphismDemo {
     displayObject(new Circle(1, "red", false));
     displayObject(new Rectangle(1, 1, "black", true));
 
+  }
     // Display geometric object properties
     public static void displayObject(GeometricObject object) {
       System.out.println("Created on " + object.getDateCreated() +
           ". Color is " + object.getColor());
-    }
   }
 }
