fba14240bbd762b17ffd185950149618ecd6119b
xorla <xorlatumaku@gmail.com>
2026-09-25 02:02:45 -0700

    Display circle and rectangle properties

---
+++ Chapter-11/PolymorphismDemo.java
@@ -0,0 +1,7 @@
+public class PolymorphismDemo {
+  public static void main(String[] args) {
+    // Display circle and rectangle properties
+    displayObject(new Circle(1, "red", false));
+    displayObject(new Rectangle(1, 1, "black", true));
+  }
+}
