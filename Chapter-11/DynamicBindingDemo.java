ea8f90a1c1c57023308b8c29e7adb1af026bfcf8
xorla <xorlatumaku@gmail.com>
2026-09-25 02:32:49 -0700

    Add polymorphic call

---
+++ Chapter-11/DynamicBindingDemo.java
@@ -0,0 +1,8 @@
+public class DynamicBindingDemo {
+  public static void main(String[] args) {
+    m(new GraduateStudent());
+    m(new Student());
+    m(new Person());
+    m(new Object());
+  }
+}
