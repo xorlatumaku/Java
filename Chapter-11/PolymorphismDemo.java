f0dffbdade0eb435140fdcde03285906d136672a
xorla <xorlatumaku@gmail.com>
2026-09-25 02:04:37 -0700

    Display geometric object properties

--- Chapter-11/PolymorphismDemo.java
+++ Chapter-11/PolymorphismDemo.java
@@ -3,5 +3,11 @@ public class PolymorphismDemo {
     // Display circle and rectangle properties
     displayObject(new Circle(1, "red", false));
     displayObject(new Rectangle(1, 1, "black", true));
+
+    // Display geometric object properties
+    public static void displayObject(GeometricObject object) {
+      System.out.println("Created on " + object.getDateCreated() +
+          ". Color is " + object.getColor());
+    }
   }
 }
