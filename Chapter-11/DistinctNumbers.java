24b844b6c84806da27ae2d69cca4ba18f3d2632d
xorla <xorlatumaku@gmail.com>
2026-09-26 03:34:26 -0700

    Read a value from the input

--- Chapter-11/DistinctNumbers.java
+++ Chapter-11/DistinctNumbers.java
@@ -4,5 +4,13 @@ import java.util.Scanner;
 public class DistinctNumbers {
   public static void main(String[] args) {
     ArrayList<Integer> list = new ArrayList<>();
+
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter integers (input ends with 0): ");
+    int value;
+
+    do {
+      value = input.nextInt();  // Read a value from the input
+    } while (value != 0);
   }
 }
