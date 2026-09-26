3922702cd179fe8c4c798ea3474f1d192202484f
xorla <xorlatumaku@gmail.com>
2026-09-26 02:48:57 -0700

    Check whether the list contains an element

--- Chapter-11/TestArrayList.java
+++ Chapter-11/TestArrayList.java
@@ -20,5 +20,6 @@ public class TestArrayList {
     // cityList now contains [London, Denver, Paris, Miami, Seoul, Tokyo]
     
     System.out.println("List size? " + cityList.size());
+    System.out.println("Is Miami in the list? " + cityList.contains("Miami"));
   }
 }
