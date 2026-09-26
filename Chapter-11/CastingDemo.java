3cada0ce639d67965b706905f4dfb23829f1ae95
xorla <xorlatumaku@gmail.com>
2026-09-25 23:06:40 -0700

    Add method for displaying object

--- Chapter-11/CastingDemo.java
+++ Chapter-11/CastingDemo.java
@@ -8,4 +8,15 @@ public class CastingDemo {
     displayObject(object1);
     displayObject(object2);
   }
+
+  // A method for displaying an object
+  public static void displayObject(Object object) {
+    if (object instanceof Circle) {
+      System.out.println("The circle area is " + ((Circle)object).getArea());
+      System.out.println("The circle diameter is " + ((Circle)object).getDiameter());
+    }
+    else if (object instanceof Rectangle) {
+      System.out.println("The rectangle area is " + ((Rectangle)object).getArea());
+    }
+  }
 }
