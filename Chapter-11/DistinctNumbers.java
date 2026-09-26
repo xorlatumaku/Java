eccef30b45f7516cdcb3ffb9647f6d3722eec308
xorla <xorlatumaku@gmail.com>
2026-09-26 03:31:51 -0700

    Create an array list

---
+++ Chapter-11/DistinctNumbers.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DistinctNumbers {
+  public static void main(String[] args) {
+    ArrayList<Integer> list = new ArrayList<>();
+  }
+}
