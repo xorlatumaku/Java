443898afea4c367dcef9e5ad8836cd5ce500f430
xorla <xorlatumaku@gmail.com>
2026-09-26 02:39:18 -0700

    Create a list to store cities

---
+++ Chapter-11/TestArrayList.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class TestArrayList {
+  public static void main(String[] args) {
+    // Create a list to store cities
+    ArrayList<String> cityList = new ArrayList<>();
+  }
+}
