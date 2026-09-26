b9c4f5aefb0661ae6356b8cd56027cff1f76ecb2
xorla <xorlatumaku@gmail.com>
2026-09-26 03:12:46 -0700

    Add two circles

--- Chapter-11/TestArrayList.java
+++ Chapter-11/TestArrayList.java
@@ -46,5 +46,9 @@ public class TestArrayList {
 
     // Create a list to store two circles
     ArrayList<Circle> list = new ArrayList<>();
+
+    // Add two circles
+    list.add(new Circle(2));
+    list.add(new Circle(3));
   }
 }
