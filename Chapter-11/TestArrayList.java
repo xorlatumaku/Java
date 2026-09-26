332426937233c1a5b6a76d3a60b10bedd42a6740
xorla <xorlatumaku@gmail.com>
2026-09-26 02:44:29 -0700

    Add London, Denver, Paris, Miami, Seoul, Tokyo to the list

--- Chapter-11/TestArrayList.java
+++ Chapter-11/TestArrayList.java
@@ -4,5 +4,19 @@ public class TestArrayList {
   public static void main(String[] args) {
     // Create a list to store cities
     ArrayList<String> cityList = new ArrayList<>();
+
+    // Add some cities in the list
+    cityList.add("London");
+    // cityList now contains [London]
+    cityList.add("Denver");
+    // cityList now contains [London, Denver]
+    cityList.add("Paris");
+    // cityList now contains [London, Denver, Paris]
+    cityList.add("Miami");
+    // cityList now contains [London, Denver, Paris, Miami]
+    cityList.add("Seoul");
+    // cityList now contains [London, Denver, Paris, Miami, Seoul]
+    cityList.add("Tokyo");
+    // cityList now contains [London, Denver, Paris, Miami, Seoul, Tokyo]
   }
 }
