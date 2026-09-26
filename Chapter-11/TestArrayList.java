d1d660c606f0525f084b7eb59ca93131251a048b
xorla <xorlatumaku@gmail.com>
2026-09-26 03:10:19 -0700

    Display the contents in the list in reverse order

--- Chapter-11/TestArrayList.java
+++ Chapter-11/TestArrayList.java
@@ -38,5 +38,10 @@ public class TestArrayList {
 
     // Display the contents in the list
     System.out.println(cityList.toString());
+
+    // Display the contents in the list in reverse order
+    for (int i = cityList.size() - 1; i >= 0; i--)
+      System.out.print(cityList.get(i) + " ");
+    System.out.println();
   }
 }
