2a852e39683aaab8505adcf792d0c3a3e5bdd919
xorla <xorlatumaku@gmail.com>
2026-09-25 22:59:57 -0700

    Create and initialize two objects

---
+++ Chapter-11/CastingDemo.java
@@ -0,0 +1,7 @@
+public class CastingDemo {
+  public static void main(String[] args) {
+    // Create and initialize two objects
+    Object object1 = new Circle(1);
+    Object object2 = new Rectangle(1, 1);
+  }
+}
