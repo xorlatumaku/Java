b6ee064f3b337748461bd75cd0cc865e1be9b062
xorla <xorlatumaku@gmail.com>
2026-09-26 02:50:00 -0700

    Check the index of an element

--- Chapter-11/TestArrayList.java
+++ Chapter-11/TestArrayList.java
@@ -21,5 +21,6 @@ public class TestArrayList {
     
     System.out.println("List size? " + cityList.size());
     System.out.println("Is Miami in the list? " + cityList.contains("Miami"));
+    System.out.println("The location of Denver in the list? " + cityList.indexOf("Denver"));
   }
 }
